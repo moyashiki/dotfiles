d282eda6a97abe29570190d58aa1011bab557d6d
moyashiki <moyashiki@gmail.com>
2015-09-20 12:17:57 +0900

    add tmux config file.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=( .vimrc .gvimrc .emacs.d .bash_profile .zshrc .zshenv .gemrc)
+DOT_FILES=( .vimrc .gvimrc .emacs.d .bash_profile .zshrc .zshenv .gemrc .tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
